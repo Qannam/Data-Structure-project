0a8aadc0f9af911c0648718f878ff2d7571539be
Qannam <Qannam@192.168.100.7>
2017-04-17 00:43:45 +0300

    test3

--- csc212Project/src/test.java
+++ csc212Project/src/test.java
@@ -23,6 +23,7 @@ public class test {
 	while(!kList.last()){
 		System.out.println(kList.retrieve().first + " " + kList.retrieve().second);
 		System.out.println("==============");
+		System.out.println("==============");
 		kList.findNext();
 	}
 	System.out.println(kList.retrieve().first + " " + kList.retrieve().second);
